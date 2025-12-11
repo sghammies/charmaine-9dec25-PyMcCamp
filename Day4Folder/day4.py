c9d22b7b0227d779eff86b405aea9c33a61aad09
sghammies <kahyong@singnet.com.sg>
2025-12-11 07:28:52 +0000

    Dec 11, 2025, 7:28 AM

--- Day4Folder/day4.py
+++ Day4Folder/day4.py
@@ -4,10 +4,10 @@
 
 ########################################################################
 # Task 1:
-counter = 0
-while counter < 10:
-    print(counter)
-    counter += 1
+# counter = 0
+# while counter < 10:
+#     print(counter)
+#     counter += 1
 
 counter = 5
 while counter < 33:
