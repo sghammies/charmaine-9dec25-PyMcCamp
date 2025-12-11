f7b1519892fa9161321f78d3a8f4569922023b90
sghammies <kahyong@singnet.com.sg>
2025-12-11 07:15:00 +0000

    Dec 11, 2025, 7:15 AM

--- Day3Folder/day3.py
+++ Day3Folder/day3.py
@@ -21,8 +21,8 @@ print("hello from day3")
 
 ########################################################################
 # Task 3:
-person1 = input("what is person a's name? ")
-person2 = input("what is person b's name? ")
+person1 = input("what is person a's age? ")
+person2 = input("what is person b's age? ")
 if int(person1) > (person2):
     print("person a is older")
 else:
