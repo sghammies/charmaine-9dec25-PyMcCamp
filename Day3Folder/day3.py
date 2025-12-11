ca76e67c751981785eefe473aefd881b0daad20d
sghammies <kahyong@singnet.com.sg>
2025-12-11 06:59:22 +0000

    Dec 11, 2025, 6:59 AM

--- Day3Folder/day3.py
+++ Day3Folder/day3.py
@@ -12,6 +12,10 @@ print("hello from day3")
 
 ########################################################################
 # Task 2:
+num1 = input("what is your first number? ")
+num2 = input("what is your second number? ")
+ans = int(num1) + int(num2) 
+print(num1 + " + " + num2 + " = " + str(ans))
 
 
 
